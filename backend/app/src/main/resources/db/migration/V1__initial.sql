e9937671c02bc40eda05f91ad0abb5388283118a
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2024-01-20 15:14:00 +0100

    feat(sql): update sql schema

--- backend/app/src/main/resources/db/migration/V1__initial.sql
+++ backend/app/src/main/resources/db/migration/V1__initial.sql
@@ -1,5 +1,30 @@
-CREATE TABLE "user" (
+create table users
+(
+    id         int GENERATED ALWAYS AS IDENTITY primary key,
     first_name varchar,
-    last_name varchar,
-    email varchar
+    last_name  varchar,
+    email      varchar,
+    is_admin   boolean
+);
+
+create table workshop
+(
+    id          int GENERATED ALWAYS AS IDENTITY primary key,
+    title       varchar                  not null,
+    description varchar,
+    start_time  timestamp with time zone not null,
+    end_time    timestamp with time zone not null,
+    capacity    int                      not null
+);
+
+create table workshop_registration
+(
+    id          int GENERATED ALWAYS AS IDENTITY primary key,
+    user_id     int,
+    workshop_id int,
+    state       int not null default 0,
+    created_at timestamp with time zone not null default current_timestamp,
+    updated_at timestamp with time zone not null default current_timestamp,
+    constraint fk_workshop foreign key (workshop_id) references workshop (id),
+    constraint fk_user foreign key (user_id) references users (id)
 );
